2be7772c8ea837a416bb3b2598ce49ce87cb24e5
Brandon Huang <hotbrandon@hotmail.com>
2025-09-24 14:59:14 +0800

    created http client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
+type Payload struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
-	response, err := http.Get("http://localhost:8080/hello")
+	payload := Payload{
+		UserName: "jack",
+		Password: "1234567",
+	}
+
+	// Marshal struct to JSON
+	jsonData, err := json.Marshal(payload)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+
+	// Make POST request
+	response, err := http.Post("http://localhost:8080/users", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
 	}
 	defer response.Body.Close()
-	fmt.Println(response)
+	if response.StatusCode == http.StatusOK {
+		data, err := io.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Response:", string(data))
+		fmt.Println("Status OK")
+	} else {
+		fmt.Println(response.StatusCode)
+	}
+
 }
--- main.go
+++ main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 )
+
+type Payload struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func randNum(w http.ResponseWriter, r *http.Request) {
+	num := rand.Intn(50)
+	fmt.Fprint(w, num)
+
+}
+
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		fmt.Fprintln(w, "Create a new user")
+		payload := Payload{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		// Example: respond with the parsed values
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"user":     payload.Username,
+			"password": payload.Password,
+		})
+		return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+
+}
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World!")
 	})
 
-	err := http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/random", randNum)
+
+	http.HandleFunc("/users", handleUser)
+
+	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		fmt.Println(err)
 		return
